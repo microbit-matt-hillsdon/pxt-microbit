3a951719c9394c72c432966ee8b7db86628efb1b
Matt Hillsdon <matt.hillsdon@microbit.org>
2024-05-31 14:42:30 +0100

    Don't assume pxt-microbit dir name

--- ci.sh
+++ ci.sh
@@ -4,14 +4,15 @@ set -euxo pipefail
 
 # Hacky CI to build this in CF Pages
 npm install -g pxt
-cd ../
-git clone git@github.com:microbit-grace/pxt.git
-cd pxt
-git checkout download-post-message
-npm install
-npm run build
-npm link
+(
+  cd ../
+  git clone git@github.com:microbit-grace/pxt.git
+  cd pxt
+  git checkout download-post-message
+  npm install
+  npm run build
+  npm link
+)
 
-cd ../pxt-microbit
 npm link ../pxt
 pxt staticpkg
