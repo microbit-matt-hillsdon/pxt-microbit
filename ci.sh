a2c60985a9376f87548f24148a5b0b9ccd652188
Robert Knight <robert@microbit.org>
2024-06-04 15:56:31 +0100

    Try to add ml-runner-poc as well

--- ci.sh
+++ ci.sh
@@ -22,7 +22,13 @@ cd libs
 git clone -b v0.3.0 git@github.com:microbit-foundation/pxt-ml-extension-poc.git
 mv pxt-ml-extension-poc machine-learning-poc
 cd machine-learning-poc
-node -e "const f = 'pxt.json'; const data = fs.readFileSync(f, 'utf8'); fs.writeFileSync(f, data.replace('*', 'file:../core').replace('Machine Learning POC', 'machine-learning-poc'))"
+node -e "const f = 'pxt.json'; const data = fs.readFileSync(f, 'utf8'); fs.writeFileSync(f, data.replace('*', 'file:../core'))"
+pxt install
+cd ..
+git clone -b v0.3.2 git@github.com:microbit-foundation/pxt-ml-runner-poc.git
+mv pxt-ml-extension-poc ml-runner-poc
+cd ml-runner-poc
+node -e "const f = 'pxt.json'; const data = fs.readFileSync(f, 'utf8'); fs.writeFileSync(f, data.replace('*', 'file:../core'))"
 pxt install
 cd ../..
 
