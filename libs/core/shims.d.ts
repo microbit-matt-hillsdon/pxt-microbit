4d22575220e1795da7d257f8c72ad7f3484a0ed5
Robert Knight <robert@microbit.org>
2025-12-04 13:26:23 +0000

    Tweak show number and text block defs to avoid commas in readout

--- libs/core/shims.d.ts
+++ libs/core/shims.d.ts
@@ -152,7 +152,7 @@ declare namespace basic {
      */
     //% help=basic/show-string
     //% weight=87 blockGap=16
-    //% block="show|string %text"
+    //% block="show string %text"
     //% async
     //% blockId=device_print_message
     //% parts="ledmatrix"
