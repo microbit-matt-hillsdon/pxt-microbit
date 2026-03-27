e406203ec01e93e862ebe26514ff7e904c61080f
Robert Knight <robert@microbit.org>
2026-03-27 15:26:33 +0000

    Align mouse and keyboard behaviour for pins

--- sim/visuals/microbit.ts
+++ sim/visuals/microbit.ts
@@ -616,7 +616,7 @@ path.sim-board {
             }
             else if (pin.mode & PinFlags.Digital) {
                 gradientValue = pin.value > 0 ? "0%" : "100%";
-                v = pin.value > 1 ? 1 : 0;
+                v = pin.value >= 1 ? 1 : 0;
                 if (text) text.textContent = v.toString();
             }
             else if (pin.mode & PinFlags.Touch) {
@@ -648,7 +648,7 @@ path.sim-board {
                     this.pins[index].setAttribute("aria-orientation", "vertical");
                     this.pins[index].setAttribute("aria-valuenow", v.toString());
                     if (text?.textContent) {
-                        this.pins[index].setAttribute("aria-valuetext", text?.textContent ?? null);
+                        this.pins[index].setAttribute("aria-valuetext", text.textContent);
                     } else {
                         this.pins[index].removeAttribute("aria-valuetext");
                     }
@@ -1479,7 +1479,7 @@ path.sim-board {
                         let state = this.board;
                         let pin = state.edgeConnectorState.pins[index];
                         let svgpin = this.pins[index];
-                        if (pin.mode & PinFlags.Input) {
+                        if (pin.mode & PinFlags.Input && !(pin.mode & PinFlags.Touch)) {
                             let cursor = svg.cursorPoint(pt, this.element, ev);
                             let v = (400 - cursor.y) / 40 * 1023
                             pin.value = Math.max(0, Math.min(1023, Math.floor(v)));
@@ -1492,7 +1492,8 @@ path.sim-board {
                         let pin = state.edgeConnectorState.pins[index];
                         let svgpin = this.pins[index];
                         U.addClass(svgpin, "touched");
-                        if (pin.mode & PinFlags.Input) {
+                        if (pin.mode & PinFlags.Input && !(pin.mode & PinFlags.Touch)) {
+                            console.log("what")
                             let cursor = svg.cursorPoint(pt, this.element, ev);
                             let v = (400 - cursor.y) / 40 * 1023
                             pin.value = Math.max(0, Math.min(1023, Math.floor(v)));
@@ -1514,10 +1515,17 @@ path.sim-board {
                         let state = this.board;
                         let pin = state.edgeConnectorState.pins[index];
 
-                        if ([37, 38, 39, 40].includes(charCode) && !(pin.mode & PinFlags.Input)) {
-                            ev.preventDefault();
-                            accessibility.setLiveContent(pxsim.localization.lf("This input is read only"));
-                            return;
+                        if ([37, 38, 39, 40].includes(charCode)) {
+                            if (!(pin.mode & PinFlags.Input)) {
+                                ev.preventDefault();
+                                accessibility.setLiveContent(pxsim.localization.lf("This input is read only"));
+                                return;
+                            }
+                            if (pin.mode & PinFlags.Touch) {
+                                // The pin is in touch mode and has button markup, not a slider.
+                                ev.preventDefault();
+                                return;
+                            }
                         };
 
                         if (charCode === 40 || charCode === 37) { // Down/Left arrow
